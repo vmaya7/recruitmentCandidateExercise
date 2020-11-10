e2497611a6a0032c42910db0e11a2316e15d16c0
Víctor M <vhms10@outlook.com>
2020-11-10 03:42:29 -0600

    New layout app

--- ui.R
+++ ui.R
@@ -4,6 +4,7 @@ library(shinydashboard)
 ui <- dashboardPage(skin = "green",
                     dashboardHeader(title = 'brand’s advertising spend', titleWidth = 270),
                     dashboardSidebar(
+                      sliderInput("slider", "Number of observations:",min =  0, max =  1, value = 0.8, step = 0.1),
                       sidebarMenu(menuItem("Linear regression ", tabName = "home", icon = icon("dashboard")),
                                   
                                   menuItem("summary", tabName = "pais", icon = icon("globe-africa"))
@@ -22,10 +23,10 @@ ui <- dashboardPage(skin = "green",
                             valueBoxOutput("value1")
                             ,valueBoxOutput("value2")
                             ,valueBoxOutput("value3")
-                          ), fluidRow(
-                            box(title = "Retention Factor [0,1]", status = "primary", solidHeader = TRUE, sliderInput("slider", "Number of observations:",min =  0, max =  1, value = 0.8, step = 0.1), height = 500), 
-                            box(title = "Lista de éxitos global", status = "primary", solidHeader = TRUE, plotOutput("plot"),  height = 500)
-                            )
+                          ), 
+                          fluidRow(
+                            box(title = "Lista de éxitos global", status = "primary", solidHeader = TRUE, plotOutput("plot"),  width=12)
+                          )
                         )
                       )
                     )
